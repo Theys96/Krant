97423d9f4f16455b93c583a5b154fc0bd1d1eb77
Theys96 <thijs@thijshavinga.nl>
2018-10-19 23:04:25 +0200

    Schrijfregels openen in nieuw tab

--- code/home.php
+++ code/home.php
@@ -18,7 +18,7 @@
             <a class="nav-link" href="?action=cats">Categorieën</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="?action=schrijfregels">Schrijfregels</a>
+            <a class="nav-link" href="?action=schrijfregels" target="_blank">Schrijfregels</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="?action=admin" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Beheer</a>

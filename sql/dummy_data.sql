41ab1f725c067e70fa505b13504d27eb717a69b8
Thijs Havinga <info@thijshavinga.nl>
2022-06-22 20:13:44 +0200

    Nieuwe database setup.

---
+++ sql/dummy_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO `articles` (`id`, `title`, `contents`, `category`, `status`, `ready`, `last_updated`) VALUES
+(1, 'Test', 'Dit is een test.', 1, 'open', 0, '2022-06-21 15:20:43'),
+(2, 'Een nieuw stukje.', 'Dit is een nieuw stukje met een aanpassing.', 1, 'open', 0, '2022-06-21 15:36:27');
+
+INSERT INTO `article_updates` (`id`, `article_id`, `update_type`, `changed_status`, `changed_title`, `changed_contents`, `changed_category`, `changed_ready`, `user`, `timestamp`) VALUES
+(1, 1, 2, 'open', 'Test', 'Dit is een test.', 1, 0, 1, '2022-06-21 15:20:43'),
+(2, 2, 2, 'open', 'Een nieuw stukje.', 'Dit is een nieuw stukje.', 1, 0, 1, '2022-06-21 15:36:27'),
+(3, 2, 4, NULL, NULL, NULL, NULL, NULL, 2, '2022-06-21 15:52:14'),
+(4, 2, 3, NULL, NULL, 'Dit is een nieuw stukje met een aanpassing.', NULL, NULL, 1, '2022-06-22 17:54:44'),
+(5, 2, 4, NULL, NULL, NULL, NULL, NULL, 2, '2022-06-22 18:01:07');
---
+++ sql/init/krant.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: db
+-- Generation Time: Jun 22, 2022 at 06:06 PM
+-- Server version: 5.7.38
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `krant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `contents` text NOT NULL,
+  `category` int(11) DEFAULT NULL,
+  `status` set('draft','open','placed','bin') NOT NULL DEFAULT 'draft',
+  `ready` tinyint(1) NOT NULL DEFAULT '0',
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_updates`
+--
+
+CREATE TABLE `article_updates` (
+  `id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `update_type` int(11) NOT NULL,
+  `changed_status` enum('draft','open','placed','bin') DEFAULT NULL,
+  `changed_title` text,
+  `changed_contents` text,
+  `changed_category` int(11) DEFAULT NULL,
+  `changed_ready` tinyint(1) DEFAULT NULL,
+  `user` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_update_types`
+--
+
+CREATE TABLE `article_update_types` (
+  `id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `author` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `article_update_types`
+--
+
+INSERT INTO `article_update_types` (`id`, `description`, `author`) VALUES
+(1, 'Draft.', 0),
+(2, 'Nieuw stukje.', 1),
+(3, 'Aanpassing.', 1),
+(4, 'Nagekeken.', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(1, 'Algemeen', 'Algemeen');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user` text NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL,
+  `user` text NOT NULL,
+  `role` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `address` text NOT NULL,
+  `message` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `perm_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `perm_level`) VALUES
+(1, 'Thijs', 3),
+(2, 'Renske', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `articles_categories` (`category`);
+
+--
+-- Indexes for table `article_updates`
+--
+ALTER TABLE `article_updates`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `article_updates_articles` (`article_id`),
+  ADD KEY `article_updates_categories` (`changed_category`),
+  ADD KEY `article_updates_users` (`user`),
+  ADD KEY `article_updates_article_update_types` (`update_type`);
+
+--
+-- Indexes for table `article_update_types`
+--
+ALTER TABLE `article_update_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `article_updates`
+--
+ALTER TABLE `article_updates`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `articles_categories` FOREIGN KEY (`category`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `article_updates`
+--
+ALTER TABLE `article_updates`
+  ADD CONSTRAINT `article_updates_article_update_types` FOREIGN KEY (`update_type`) REFERENCES `article_update_types` (`id`),
+  ADD CONSTRAINT `article_updates_articles` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`),
+  ADD CONSTRAINT `article_updates_categories` FOREIGN KEY (`changed_category`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `article_updates_users` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

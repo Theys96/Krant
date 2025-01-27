967e5170034ca6decb2aa8f0b922e5fb75c77533
FJDijkstra <167628132+FJDijkstra@users.noreply.github.com>
2025-01-27 16:15:33 +0100

    body toegevoegd
    body toegevoegd aan mail

--- www/assets/js/editor.js
+++ www/assets/js/editor.js
@@ -16,6 +16,6 @@ function sendMail(){
     var title = document.getElementById('title').value
     var author = document.getElementById('user').value
     document.getElementById('context').value = 'Foto(s) door ' + author + ' gemaild\n' + document.getElementById('context').value;
-    window.location.href = 'mailto:' + mail + '?subject= FOTO ' + title + ', AUTEUR ' + author; 
+    window.location.href = 'mailto:' + mail + '?subject= FOTO ' + title + ', AUTEUR ' + author + '&body=Voeg je foto(s) toe!'; 
 
 }

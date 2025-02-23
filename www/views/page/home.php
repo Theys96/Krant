3102ea3524b3d178385a8010c238952d6593782d
Thijs Havinga <info@thijshavinga.nl>
2025-02-23 14:15:59 +0100

    Menu gefixt

--- www/views/page/home.php
+++ www/views/page/home.php
@@ -7,53 +7,48 @@
  * @var string $content
  */
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="?action=list"><?php echo $username; ?></a>
-    <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarsExampleDefault"
-            aria-controls="navbarsExampleDefault"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-    >
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-lg bg-body-secondary fixed-top" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="?action=list"><?php echo $username; ?></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="?action=list">Stukjes</a>
-            </li>
-            <?php if (1 == $role || 3 == $role) { ?>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="?action=create">Schrijf</a>
+                    <a class="nav-link" href="?action=list">Stukjes</a>
                 </li>
-            <?php } ?>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="?action=admin" id="dropdown01" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">Beheer</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="?action=overview">Overzicht</a>
-                    <a class="dropdown-item" href="?action=categories">Categorieën</a>
-                    <a class="dropdown-item" href="?action=drafts">Drafts</a>
-                    <a class="dropdown-item" href="?action=bin">Prullenbak</a>
-                    <a class="dropdown-item" href="?action=placed">Geplaatst</a>
-                    <?php if (3 == $role) { ?>
-                        <a class="dropdown-item" href="?action=editions">Edities</a>
-                        <a class="dropdown-item" href="?action=users">Gebruikers</a>
-                        <a class="dropdown-item" href="?action=feedbacklist">Feedback</a>
-                        <a class="dropdown-item" href="?action=configuratie">Configuratie</a>
-                    <?php } ?>
-                </div>
-            </li>
-        </ul>
-        <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="?action=schrijfregels" target="_blank">Schrijfregels</a></li>
-            <li class="nav-item"><a class="nav-link" href="?action=feedback">Feedback</a></li>
-            <li class="nav-item"><a class="nav-link" href="?action=logout">Uitloggen</a></li>
-        </ul>
+                <?php if (1 == $role || 3 == $role) { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?action=create">Schrijf</a>
+                    </li>
+                <?php } ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Beheer
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="?action=overview">Overzicht</a>
+                        <a class="dropdown-item" href="?action=categories">Categorieën</a>
+                        <a class="dropdown-item" href="?action=drafts">Drafts</a>
+                        <a class="dropdown-item" href="?action=bin">Prullenbak</a>
+                        <a class="dropdown-item" href="?action=placed">Geplaatst</a>
+                        <?php if (3 == $role) { ?>
+                            <a class="dropdown-item" href="?action=editions">Edities</a>
+                            <a class="dropdown-item" href="?action=users">Gebruikers</a>
+                            <a class="dropdown-item" href="?action=feedbacklist">Feedback</a>
+                            <a class="dropdown-item" href="?action=configuratie">Configuratie</a>
+                        <?php } ?>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="?action=schrijfregels" target="_blank">Schrijfregels</a></li>
+                <li class="nav-item"><a class="nav-link" href="?action=feedback">Feedback</a></li>
+                <li class="nav-item"><a class="nav-link" href="?action=logout">Uitloggen</a></li>
+            </ul>
+        </div>
     </div>
 </nav>
 

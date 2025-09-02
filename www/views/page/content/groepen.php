72bab298b743574bffbd482aaccdcffd67e74bd1
FJDijkstra <167628132+FJDijkstra@users.noreply.github.com>
2025-09-02 18:52:16 +0200

    Beter overzicht
    de pdf wordt nu beter weergeven

--- www/views/page/content/groepen.php
+++ www/views/page/content/groepen.php
@@ -1,6 +1,2 @@
-<h2>Groepenoverzicht</h2>
-<object class="pdf" 
-        data="\assets\img\scoutinggroepen.pdf"
-        width="100%"
-        height="100%">
-</object>
+
+<embed src="\assets\img\scoutinggroepen.pdf" style="position:absolute; left: 0; top: 0;" width="100%" height="100%" type="application/pdf">
